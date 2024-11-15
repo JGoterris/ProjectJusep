56f3f2896036cb3f31cd9d87905d39b95d50be53
Jesús Goterris <jgoterris24@gmail.com>
2024-11-15 09:31:15 +0100

    Initial Menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+}
+
