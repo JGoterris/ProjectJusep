bae225907df456873f559dfb7d2bcbea234b7732
Hector-Martínez <al426185@uji.es>
2024-12-04 19:56:52 +0100

    Updated Wizard an animation controller

--- Assets/Scripts/Enemies/DarkWizardEnemy.cs
+++ Assets/Scripts/Enemies/DarkWizardEnemy.cs
@@ -8,54 +8,92 @@ public class DarkWizardEnemy : MonoBehaviour
     private Animator animator;
     private NavMeshAgent agent;
     private DarkWizardMagicSystem magicSystem;
+    private HealthComponent health;
+
+    // Modificable variables (on engine)
     [SerializeField] private float castSpellDelay = 3; // Seconds
-    public Transform target;
+    [SerializeField] private Transform target;
     [SerializeField] private float visionRadius = 8;
+    // When the target is at 3 units or less of distance from the mage, it flees away
+    [SerializeField] private float scareRadius = 2;
+    [SerializeField] private float combatRadius = 6;
 
     // Attack variables
     private float castSpellTimer = 0;
-    private float lastSpellTimer = 0;
 
     // Patrol variables
+    [SerializeField] private float patrolRange = 4;
+
+    private Vector3 patrolDestination;
+
+    // State variables
+    private bool m_attacking = false;
+    private bool m_patroling = false;
+    private bool m_scared = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         magicSystem = GetComponent<DarkWizardMagicSystem>();
+        health = GetComponent<HealthComponent>();
+
+        m_patroling = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         castSpellTimer += Time.deltaTime;
 
-        if (Vector3.Distance(agent.transform.position, target.position) <= visionRadius)
-        {
-            Attack();
-        } else
-        {
-            Patrol();
-        }
+        float distance = Vector3.Distance(agent.transform.position, target.position);
 
-        MovementReaction();
+        
+        
     }
 
-    void Attack()
+    private void Attack()
     {
-        if (castSpellTimer - lastSpellTimer >= castSpellDelay)
+        
+        
+    }
+
+    private void Chase()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void Patrol()
+    {
+        if (agent.transform.position.Equals(patrolDestination))
         {
-            magicSystem.CastFireBall();
-            lastSpellTimer = castSpellTimer;
+            if (RandomPoint(agent.transform.position, patrolRange, out patrolDestination))
+            {
+                agent.SetDestination(patrolDestination);
+            }
         }
     }
 
-    void Patrol()
+    private bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; // Random point in a shpere
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            result = hit.position;
+            return true;
+        }
+        result = Vector3.zero;
+        return false;
+    }
+
+    private void RunAway()
     {
 
     }
 
-    void MovementReaction()
+    public void SlowDown()
     {
 
     }
