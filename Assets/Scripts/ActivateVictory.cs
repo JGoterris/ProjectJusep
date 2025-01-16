04d965cf3942fc29dff940b54a459b4e16bd2e88
Jesús Goterris <jgoterris24@gmail.com>
2025-01-16 19:13:26 +0100

    Falta borrar cosass

--- Assets/Scripts/ActivateVictory.cs
+++ Assets/Scripts/ActivateVictory.cs
@@ -6,13 +6,11 @@ using UnityEngine;
 public class ActivateVictory : MonoBehaviour
 {
     public GameObject victoryMenu;
-    public GameObject pause;
 
     public void Victory()
     {
         Time.timeScale = 0f;
         victoryMenu.SetActive(true);
-        pause.SetActive(true);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
