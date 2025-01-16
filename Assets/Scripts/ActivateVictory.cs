30bea4b098b6728ac1d7588155124a12efd4898c
Jesús Goterris <jgoterris24@gmail.com>
2025-01-16 19:45:28 +0100

    Haciendo pruebas

--- Assets/Scripts/ActivateVictory.cs
+++ Assets/Scripts/ActivateVictory.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class ActivateVictory : MonoBehaviour
 {
     public GameObject victoryMenu;
+    [SerializeField] private PauseScript pauseScript;
 
     public void Victory()
     {
         Time.timeScale = 0f;
         victoryMenu.SetActive(true);
+        pauseScript.yaNoSePuedePausar();
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
